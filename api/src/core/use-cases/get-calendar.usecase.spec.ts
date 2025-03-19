4b1fe55561c6521577c096ae7ebf13858ab77272
Walid SADAOUI <walidsadaoui@hotmail.fr>
2025-03-19 22:15:21 +0000

    feat(calendar): get calendar by league

--- api/src/core/use-cases/get-calendar.usecase.spec.ts
+++ api/src/core/use-cases/get-calendar.usecase.spec.ts
@@ -1,16 +1,15 @@
-import { GetCalendarUseCase } from "./get-calendar.usecase";
+import { FakeCalendarStore, GetCalendarUseCase } from "./get-calendar.usecase";
 
 describe("Get Calendar UseCase", () => {
+  const fakeCalendarStore = new FakeCalendarStore();
   it("should return a list of games", () => {
-    const getCalendarUseCase = new GetCalendarUseCase();
+    const getCalendarUseCase = new GetCalendarUseCase(fakeCalendarStore);
     const calendar = getCalendarUseCase.execute();
-    expect(calendar).toEqual([{ homeTeam: "PSG", awayTeam: "Liverpool" }]);
+    expect(calendar).toEqual(fakeCalendarStore.getCalendar());
   });
   it("should return a list of games for specified league", () => {
-    const getCalendarUseCase = new GetCalendarUseCase();
+    const getCalendarUseCase = new GetCalendarUseCase(fakeCalendarStore);
     const calendar = getCalendarUseCase.execute("Ligue 1");
-    expect(calendar).toEqual([
-      { homeTeam: "PSG", awayTeam: "Olympique de Marseille" },
-    ]);
+    expect(calendar).toEqual(fakeCalendarStore.getCalendar("Ligue 1"));
   });
 });
--- api/src/core/use-cases/get-calendar.usecase.ts
+++ api/src/core/use-cases/get-calendar.usecase.ts
@@ -1,9 +1,21 @@
 export class GetCalendarUseCase {
-  constructor() {}
+  constructor(private readonly calendarStore: CalendarStore) {}
 
   execute(league?: string) {
+    return this.calendarStore.getCalendar(league);
+  }
+}
+
+export class FakeCalendarStore implements CalendarStore {
+  constructor() {}
+
+  getCalendar(league?: string) {
     if (league === "Ligue 1")
       return [{ homeTeam: "PSG", awayTeam: "Olympique de Marseille" }];
     return [{ homeTeam: "PSG", awayTeam: "Liverpool" }];
   }
+}
+
+interface CalendarStore {
+  getCalendar(league?: string): void;
 }
