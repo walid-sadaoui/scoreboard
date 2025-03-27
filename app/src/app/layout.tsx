460438ac011ae8590f853293418f153ef2ae8137
Walid SADAOUI <walidsadaoui@hotmail.fr>
2025-03-27 17:08:14 +0000

    chore: init redux

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { store } from "./store";
+import { Provider } from "react-redux";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Provider store={store}>{children}</Provider>
       </body>
     </html>
   );
