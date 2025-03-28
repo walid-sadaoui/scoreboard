f7089fb2ad456989992c6ea43af097b28abe725c
WALID SADAOUI <walidsadaoui@hotmail.fr>
2025-03-28 12:05:04 +0000

    chore(next): enabled polling for hot reload

--- app/next.config.ts
+++ app/next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpackDevMiddleware: (config: any) => {
+    config.watchOptions = {
+      poll: 1000, // Vérifie les changements toutes les secondes
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
