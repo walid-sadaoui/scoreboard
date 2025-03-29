180f45a79dce21dc802bf674d71dab4a4dc0076d
WALID SADAOUI <walidsadaoui@hotmail.fr>
2025-03-29 22:27:55 +0000

    fix(backend/calendar): update imports

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { CalendarController } from "./modules/calendar/controllers/calendar.controller";
+import { GetCalendarUseCase } from "./modules/calendar/use-cases/get-calendar.usecase";
 
 @Module({
   imports: [],
   controllers: [AppController, CalendarController],
-  providers: [AppService],
+  providers: [AppService, GetCalendarUseCase],
 })
 export class AppModule {}
