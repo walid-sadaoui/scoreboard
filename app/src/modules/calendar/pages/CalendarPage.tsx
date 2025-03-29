ca5a0d61cd901e9929f3570165719f0f4699c843
WALID SADAOUI <walidsadaoui@hotmail.fr>
2025-03-29 14:55:46 +0000

    feat(calendar): add calendar event card

--- app/src/modules/calendar/pages/CalendarPage.tsx
+++ app/src/modules/calendar/pages/CalendarPage.tsx
@@ -1,7 +1,37 @@
 "use client";
 
 import React from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export const CalendarPage: React.FC = () => {
-  return <main>Calendar</main>;
+  const FAKE_EVENTS = [
+    {
+      homeTeam: "Olympique Lyonnais",
+      awayTeam: "Olympique de Marseille",
+      league: "Ligue 1",
+    },
+    {
+      homeTeam: "Paris Saint-Germain",
+      awayTeam: "Liverpool FC",
+      league: "Champions League",
+    },
+  ];
+
+  return (
+    <main className="m-auto">
+      {FAKE_EVENTS.map((event) => {
+        return (
+          <Card>
+            <CardHeader>
+              <CardTitle>{event.league}</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>{event.homeTeam}</p>
+              <p>{event.awayTeam}</p>
+            </CardContent>
+          </Card>
+        );
+      })}
+    </main>
+  );
 };
