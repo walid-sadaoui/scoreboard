462519a18ddffc0c39246339ca157adb1a55627c
WALID SADAOUI <walidsadaoui@hotmail.fr>
2025-03-29 11:48:30 +0000

    chore: use WATCHPACK_POLLING to enable hot reload

--- app/next.config.ts
+++ app/next.config.ts
@@ -2,13 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpackDevMiddleware: (config: any) => {
-    config.watchOptions = {
-      poll: 1000, // Vérifie les changements toutes les secondes
-      aggregateTimeout: 300,
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
